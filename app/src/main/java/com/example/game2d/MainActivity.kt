019a6f4ab825dd7b04592665bbdd99eb79b6600b
Aurora0043 <940403army@gmail.com>
2024-11-18 15:58:16 +0800

    v4

--- app/src/main/java/com/example/game2d/MainActivity.kt
+++ app/src/main/java/com/example/game2d/MainActivity.kt
@@ -6,8 +6,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -19,10 +21,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.IntOffset
 import com.example.game2d.ui.theme.Game2DTheme
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,8 +35,10 @@ class MainActivity : ComponentActivity() {
             Game2DTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+                    val screenW = resources.displayMetrics.widthPixels
+                    val screenH = resources.displayMetrics.heightPixels
                     val game = Game(GlobalScope)
-                    Start(m = Modifier.padding(innerPadding),game)
+                    Start(m = Modifier.padding(innerPadding),game,screenW)
                 }
             }
         }
@@ -41,9 +46,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Start(m: Modifier, game: Game) {
+fun Start(m: Modifier, game: Game, screenW: Int) {
     val counter by game.state.collectAsState()
     var counter2 by remember { mutableStateOf(0) }
+
+    Image(
+        painter = painterResource(id = R.drawable.forest),
+        contentDescription = "背景圖",
+        contentScale = ContentScale.FillBounds, //縮放符合螢幕寬度
+        modifier = Modifier
+            .offset { IntOffset(-counter, 0) }
+    )
+    Image(
+        painter = painterResource(id = R.drawable.forest),
+        contentDescription = "背景圖2",
+        contentScale = ContentScale.FillBounds, //縮放符合螢幕寬度
+        modifier = Modifier
+            .offset { IntOffset(-counter+screenW, 0) }
+    )
 
     Row {
         Button(
